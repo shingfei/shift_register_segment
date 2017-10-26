e1a97299757b82827870a18c0dac1fd9f531f645
shingfei <0915328@hr.nl>
2017-10-26 18:40:55 +0200

    Update shift_register_segment.ino

--- shift_register_segment.ino
+++ shift_register_segment.ino
@@ -18,9 +18,19 @@ int buttonStateUp = 0;
 int buttonStateDown = 0;
 boolean currentStateUp;
 boolean currentStateDown;
+//////////Hall effect Sensor
+int hall = 3;
+int hallState = 0;
+long previous;
+
+///// Floor
+int whatFloor = 2;
+
+
 //In Bits Floor 0 = 63, F1 = 6, F2 = 91, F3 = 79, F4= 102 etc etc...
 //Bits = 1(segment a), 2(segment b), 4 (segment c), 8 (d), 16 (e) , 32 (f) ,64(g), 128(DP)
-int floors[] = {63,6,91,79,102,109,125,7,103}; 
+int floors[] = {63,6,91,79,102,109,125,7,127,103}; 
+int i = 0;
 
 void setup() {
   //set pins to output so you can control the shift register
@@ -34,6 +44,7 @@ void setup() {
   pinMode(redPin,OUTPUT);
   pinMode(greenPin,OUTPUT);
   pinMode(bluePin,OUTPUT);
+  pinMode(hall,INPUT);
 }
  
  void setColor(int red, int green, int blue)
@@ -50,46 +61,103 @@ void loop() {
 digitalWrite(latchPin, LOW);
 buttonStateUp = digitalRead(buttonUp);
 buttonStateDown = digitalRead(buttonDown);
+hallState = digitalRead(hall);
+setColor(150, 255,255);
+
+ 
+if(currentStateUp == true || currentStateDown == true)
+  {
+    if(hallState == LOW && i == whatFloor) 
+    {
+      setColor(150, 255, 255);
+      digitalWrite(ledDown,LOW);
+      digitalWrite(ledUp,LOW);
+      doorDelay();
+      setColor(255, 255,150);
+    }
+    
+     else
+     {
+      minusDelay();
+     }
+  }
+
+ else if(currentStateUp == false || currentStateDown == true)
+ {
+   if(hallState == LOW) 
+    {
+     setColor(255, 255,150);
+      doorDelay();
+    }
+     else
+     {
+      minusDelay();
+     }
+ }
+
 if(buttonStateUp == LOW && currentStateUp == true)
 {
-  setColor(0, 255, 255);
-  digitalWrite(ledUp,LOW);
+  i++;
   currentStateUp = false;
-  shiftOut(dataPin, clockPin, MSBFIRST, floors[1]);  
+  digitalWrite(ledUp,LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, floors[i]);  
   digitalWrite(latchPin, HIGH);
-  delay(200);
+  buttonDelay();
 }
 else if(buttonStateUp== LOW && currentStateUp == false)
 {
-  setColor(255, 255, 150);
+  i++;
   currentStateUp = true;
   digitalWrite(ledUp,HIGH); 
-  shiftOut(dataPin, clockPin, MSBFIRST, floors[0]);  
+  shiftOut(dataPin, clockPin, MSBFIRST, floors[i]);  
   digitalWrite(latchPin, HIGH);
-  delay(200);
+  buttonDelay();
 }
  if(buttonStateDown == LOW && currentStateDown == true)
 {
-  setColor(150, 255, 255);
-  digitalWrite(ledDown,LOW);
+  i = 0;
   currentStateDown = false;
-  shiftOut(dataPin, clockPin, MSBFIRST, floors[1]);  
+  digitalWrite(ledDown,LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, floors[i]);  
   digitalWrite(latchPin, HIGH);
-  delay(200);
+  buttonDelay();
 }
 else if(buttonStateDown== LOW && currentStateDown == false)
 {
-  setColor(255, 255,150);
+  i = 2;
   currentStateDown = true;
   digitalWrite(ledDown,HIGH); 
+  shiftOut(dataPin, clockPin, MSBFIRST, floors[i]);  
+  digitalWrite(latchPin, HIGH);
+    buttonDelay();
+}
+if(i>=9)
+{
   shiftOut(dataPin, clockPin, MSBFIRST, floors[0]);  
   digitalWrite(latchPin, HIGH);
-  delay(200);
+  i=0;
+  minusDelay();
 }
 else
 {
-
-delay(1);
+minusDelay();
+}
 }
 
+int minusDelay()
+{
+  previous = millis();
+  while(millis()-previous <= 1){};
+}
+
+int buttonDelay()
+{
+  previous = millis();
+  while(millis()-previous <= 300){};
+}
+
+int doorDelay()
+{
+  previous = millis();
+  while(millis()-previous <= 5000){};
 }
