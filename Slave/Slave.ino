00fddca474ae1d431df7f1a18a04b4bc03907af8
shingfei <0915328@hr.nl>
2017-11-27 22:36:52 +0100

    Add files via upload

--- Slave/Slave.ino
+++ Slave/Slave.ino
@@ -1,27 +1,29 @@
-// Wire Slave Receiver
-// by Nicholas Zambetti <http://www.zambetti.com>
-//Com 8/4
-// Demonstrates use of the Wire library
-// Receives data as an I2C/TWI slave device
-// Refer to the "Wire Master Writer" example for use with this
-
-// Created 29 March 2006
-
-// This example code is in the public domain.
-
-
 #include <Wire.h>
 
+int testButton =12;
+int buttonState =0;
+const int slaveAdress = 8;
+long previous;
+boolean set = false;
+int response = 1;
+
 void setup() {
-  Wire.begin(9);                // join i2c bus with address #8
+  pinMode(testButton,INPUT);
+  Wire.begin(slaveAdress);                // join i2c bus with address #8
   Wire.onReceive(receiveEvent); // register event
   Wire.onRequest(requestEvent);
   Serial.begin(9600);           // start serial for output
 }
 
 void loop() {
-  
-  delay(100);
+  buttonStates();
+  //setSR();
+  minusDelay();
+}
+
+void buttonStates()
+{
+  buttonState = digitalRead(testButton);
 }
 
 // function that executes whenever data is received from master
@@ -36,9 +38,33 @@ void receiveEvent(int howMany) {
 
 }
 
+void setSR()
+{
+  if(buttonState == HIGH && set == true)
+  {
+  set = false;
+  Serial.println("recieve");
+  minusDelay();
+  }
+  if(buttonState == HIGH && set == false)
+  {
+  set = true;
+  Serial.println("recieve");
+  minusDelay();
+  }
+}
+
+
 // function that executes whenever data is requested by master
 // this function is registered as an event, see setup()
 void requestEvent() {
-  Wire.write(2); // respond with message of 6 bytes
+  Wire.write(response); // respond with message of 6 bytes
   // as expected by master
 }
+
+int minusDelay()
+{
+  previous = millis();
+  while(millis()-previous <= 500){};
+}
+
